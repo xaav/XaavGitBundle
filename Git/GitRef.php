be0451b345fff164dbc4f5b65e1678ce391736a1
xaav <xaav@xaav.tk>
2011-08-05 13:20:18 -0500

    Added test git repo.

---
+++ Git/GitRef.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Xaav\GitBundle\Git;
+
+/**
+ * Represents any git ref, including branches.
+ */
+class GitRef
+{
+    /**
+     * Gets the object (usually a commit) that this ref points to.
+     */
+    public function getObject()
+    {
+        //
+    }
+}
