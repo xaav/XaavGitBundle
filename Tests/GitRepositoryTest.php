9cfba51b591e9124747941903c9dbd32df2ae457
xaav <xaav@xaav.tk>
2011-08-05 15:57:55 -0500

    Updated test.

--- Tests/GitRepositoryTest.php
+++ Tests/GitRepositoryTest.php
@@ -2,6 +2,8 @@
 
 namespace Xaav\GitBundle\Tests;
 
+use Xaav\GitBundle\Git\Binary;
+
 use Xaav\GitBundle\Git\GitRepository;
 
 class GitRepositoryTest extends \PHPUnit_Framework_TestCase
@@ -11,6 +13,6 @@ class GitRepositoryTest extends \PHPUnit_Framework_TestCase
         $repo = new GitRepository(__DIR__.'/test.git');
 
         $this->assertEquals($repo->getTip('master'), $repo->getTip());
-        $this->assertEquals($repo->getTip(), '549efd7972e9959fdfef9c02744eabc21913bd7a');
+        $this->assertEquals($repo->getTip(), Binary::sha1_bin('549efd7972e9959fdfef9c02744eabc21913bd7a'));
     }
 }
