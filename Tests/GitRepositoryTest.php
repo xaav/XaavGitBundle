6f8b8575e150155154f524635f2e7cf63bff2a44
xaav <xaav@xaav.tk>
2011-08-05 13:42:38 -0500

    Updated test.

--- Tests/GitRepositoryTest.php
+++ Tests/GitRepositoryTest.php
@@ -2,10 +2,15 @@
 
 namespace Xaav\GitBundle\Tests;
 
+use Xaav\GitBundle\Git\GitRepository;
+
 class GitRepositoryTest extends \PHPUnit_Framework_TestCase
 {
     public function testRepository()
     {
+        $repo = new GitRepository(__DIR__.'/test.git');
 
+        $this->assertEquals($repo->getTip('master'), $repo->getTip());
+        $this->assertEquals($repo->getTip(), '549efd7972e9959fdfef9c02744eabc21913bd7a');
     }
 }
